31c9b7e444a4159781f1db9a995b416eb6b3964b
Dan <danbobs27@gmail.com>
2021-08-18 17:30:37 +0100

    Stubbed out APIs and Rest client calls

---
+++ APIGateway/Controllers/APIGatewayController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Snoop.APIGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APIGatewayController : ControllerBase
+    {
+        private readonly ILogger<APIGatewayController> _logger;
+
+        public APIGatewayController(ILogger<APIGatewayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("Encrypt")]
+        public string Encrypt([FromBody] string stringToEncrypt)
+        {
+            return $"{stringToEncrypt} - encrypted";
+        }
+
+        [HttpPost]
+        [Route("Decrypt")]
+        public string Decrypt([FromBody] string stringToDecrypt)
+        {
+            return $"{stringToDecrypt} - decrypted";
+        }
+
+
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ EncryptionService/Controllers/EncryptionServiceController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Snoop.EncryptionService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EncryptionServiceController : ControllerBase
+    {
+        private readonly ILogger<EncryptionServiceController> _logger;
+
+        public EncryptionServiceController(ILogger<EncryptionServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("Encrypt")]
+        public string Encrypt([FromBody] string stringToEncrypt)
+        {
+            return $"{stringToEncrypt} - encrypted";
+        }
+
+        [HttpPost]
+        [Route("Decrypt")]
+        public string Decrypt([FromBody] string stringToDecrypt)
+        {
+            return $"{stringToDecrypt} - decrypted";
+        }
+
+        [HttpGet]
+        [Route("RotateKey")]
+        public IActionResult RotateKey()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
